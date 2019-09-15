d85d205dafd0fc6cd77437c0ddc03ae33410e0af
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-09-15 19:24:42 +0600

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Info Page</title>
+    <link rel = "stylesheet" type = "text/css" href = "style.css" />
 </head>
 <body>
+
+    <div id="personalinfo">
+        <p>
+            <h3>Personal Info: (Part 01)</h3>
+            <h4>Name: S. M. Ashraf Kabir</h4>
+            <b>Age: </b> 23<br>
+            <b>University:</b> BRAC University <br>
+            <b>Location:</b> <i>Dhaka</i> <br>
+        </p>
+    </div>
+    <br><br>
+    <div id="links">
+        <h3>Set of links: (Part 02)</h3>
+        <p>
+            <h4></h4>
+        </p>
+    </div>
     
 </body>
 </html>
